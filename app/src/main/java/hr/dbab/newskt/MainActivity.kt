614a615d66630341559d539a261680150ab24e4c
babic <babdan5@gmail.com>
2020-06-17 22:06:02 +0200

    Initial commit

---
+++ app/src/main/java/hr/dbab/newskt/MainActivity.kt
@@ -0,0 +1,11 @@
+package hr.dbab.newskt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
